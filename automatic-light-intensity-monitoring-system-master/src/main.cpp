554c933dccd6b40cdd1e3d7b8b4f3c87a00f3830
Abhinav Kumar Sharma <abhinav.sharma_cs.aiml24@gla.ac.in>
2026-02-05 08:43:25 +0530

    DONE

--- automatic-light-intensity-monitoring-system-master/src/main.cpp
+++ automatic-light-intensity-monitoring-system-master/src/main.cpp
@@ -1,9 +1,22 @@
 #include <Arduino.h>
 #define ldrPin A0
+
 void setup() {
 // write your initialization code here
+    Serial.begin(9600);
+    pinMode(ldrPin, INPUT);
+    int ldrValue = analogRead(ldrPin);
+    Serial.print("LDR Test: ");
+    Serial.println(ldrValue);
 }
 
 void loop() {
 // write your code here
+    int ldrValue = analogRead(ldrPin);
+    int lightPercentage = map(ldrValue, 0, 1023, 0, 100);
+    Serial.print("Light Intensity: ");
+    Serial.print(lightPercentage);
+    Serial.println("%");
+    delay(1000);
+
 }
